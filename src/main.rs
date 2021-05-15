940d9b6ef696f7896fc32f01238c58bb76d328ec
Paulo <elie9alb@gmail.com>
2021-05-15 00:28:25 -0300

    Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+static HELLO: &[u8] = b"Hello World!";
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -10,5 +12,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     panic!();
 }
